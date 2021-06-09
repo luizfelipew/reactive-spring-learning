c9069636b13b8b6d1a74e72173eddf762e64391a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-06-09 15:14:37 -0300

    create Domain Model and Repositories

---
+++ netflux-reactive/src/main/java/guru/springframework/netflux/repositories/MovieRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.netflux.repositories;
+
+import guru.springframework.netflux.domain.Movie;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
+
+}
